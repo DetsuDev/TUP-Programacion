c02811510be03c4df53db517bc7ddb1fd3ee5a2b
Tomas Bottelli <tomibottelli21@gmail.com>
2024-03-27 19:40:43 -0300

    test

---
+++ programacion_I/unidad_02/Actividad_20.cpp
@@ -0,0 +1,16 @@
+// Autor: Tomas Bottelli
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int edad, altura;
+
+    cout << "Ingrese la edad de 5 personas: ";
+    cin >> edad;
+    cout << "-\n";
+	main();
+
+	return 0;
+}
